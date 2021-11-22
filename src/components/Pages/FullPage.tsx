1415b3de966e420899a565301e5b4a7e4cbbfc91
Sergey <dev.zaycev@gmail.com>
2021-11-22 13:00:22 +0200

    components/Page: add FullPage wrapper

---
+++ src/components/Pages/FullPage.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import styled from "styled-components";
+
+
+const StyledFullPageWrapper = styled.div`
+    width: 100%;
+    padding: 21px;
+`;
+
+const StyledFullPage = styled.div`
+    background: white;
+    border-radius: 28px;
+    padding: 30px 21px;
+    box-shadow: 0px 10px 20px rgba(0, 0, 0, 0.06);
+`;
+
+const FullPage: React.FC<{children?: React.ReactNode}> = ({children}) => {
+    return  <StyledFullPageWrapper>
+                <StyledFullPage>
+                    {children}
+                </StyledFullPage>
+            </StyledFullPageWrapper>
+}
+
+export default FullPage;
