c7ba1e77d1f23645c531ca5d900e2b97a7bcd9b3
Sergey <dev.zaycev@gmail.com>
2021-12-08 16:10:33 +0200

    implements: add TokenIcon component to view token logo

---
+++ src/uikit/TokenIcon/index.tsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from 'react';
+import Token from '../../core/Token';
+
+interface TokenIconAttributes extends React.ImgHTMLAttributes<HTMLImageElement> {
+  size?: number;
+  token: Token;
+}
+
+function existsLocal(name: string) {
+  return existsUrl(`/icons/tokens/${name}.svg`);
+}
+
+function existsUrl(url: string) {
+  const http = new XMLHttpRequest();
+
+  http.open('HEAD', url, false);
+  http.send();
+
+  return http.status != 404;
+}
+
+const TokenIcon = (props: TokenIconAttributes) => {
+  const { token, size = 30 } = props;
+  const [tokenUrl, setTokenUrl] = useState(`/assets/svg/undefined_token.svg`);
+  useEffect(() => {
+    if (token.icon && existsUrl(token.icon)) {
+      setTokenUrl(token.icon);
+    } else if (existsLocal(token.contract)) {
+      setTokenUrl(token.icon);
+    }
+  }, []);
+
+  return <img src={tokenUrl} {...props} height={size} width={size}></img>;
+};
+
+export default TokenIcon;
