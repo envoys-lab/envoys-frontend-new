e89bf2682f7f8b766da0f8e114103648915f479a
Sergey <dev.zaycev@gmail.com>
2021-12-13 16:44:19 +0200

    implements: release final useModal hook

--- src/hooks/useModal.ts
+++ src/hooks/useModal.ts
@@ -1,9 +1,25 @@
+import { ModalContext } from 'components/Modal/ModalContext';
 import React from 'react';
 
-const useModal = () => {
-  const [state, setState] = React.useState(null);
-  return React.useMemo(() => {
-    return [state, setState];
-  }, []);
+const useModal = (): [boolean, (content?: React.ReactNode) => void, React.ReactNode] => {
+  const [modal, setModal] = React.useState(false);
+  const [modalContent, setModalContent] = React.useState<React.ReactNode>(undefined);
+  const handleModal = (content?: React.ReactNode) => {
+    if (content) {
+      setModal(true);
+      setModalContent(content);
+    } else {
+      setModal(false);
+      setModalContent(undefined);
+    }
+  };
+
+  return [modal, handleModal, modalContent];
 };
-export default useModal;
+
+const useModalContent = (): [React.ReactNode, (content?: React.ReactNode) => void] => {
+  const context = React.useContext(ModalContext);
+  return [context.modalContent, context.handleModal];
+};
+
+export { useModalContent, useModal };
